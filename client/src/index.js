41980eaca93547af6c8226cb12969092526f823c
morelir <58606266+morelir@users.noreply.github.com>
2022-08-25 13:03:19 +0300

    Update index.js

--- client/src/index.js
+++ client/src/index.js
@@ -8,15 +8,13 @@ import store from "./shared/store/index";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <React.StrictMode>
-    <Provider store={store}>
-      <App />
-    </Provider>
-  </React.StrictMode>
+  <Provider store={store}>
+    <App />
+  </Provider>
 );
-{
+
   /* <React.StrictMode></React.StrictMode> */
-}
+
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
