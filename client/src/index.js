c2627bd433f2a80b183a159b35e40bad58fbc617
morelir <58606266+morelir@users.noreply.github.com>
2023-12-27 20:25:01 +0200

    update

--- client/src/index.js
+++ client/src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { Provider } from "react-redux";
 import store from "./shared/store/index";
 
@@ -12,11 +11,3 @@ root.render(
     <App />
   </Provider>
 );
-
-  /* <React.StrictMode></React.StrictMode> */
-
-
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
